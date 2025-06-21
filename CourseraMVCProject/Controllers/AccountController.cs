59b8ec7db3379092507e6ec26d423b12789e7167
Azima <azimaqadirli@gmail.com>
2025-06-22 02:33:51 +0400

    Add project files.

---
+++ CourseraMVCProject/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using CourseraMVCProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseraMVCProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.UserName == "admin" && model.Password == "admin123.")
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Username or Password is invalid");
+                    return View(model);
+                }
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+    }
+}
